147e61de73086af38a284e81fd4c017b59ae2902
soltia48 <soltia48@protonmail.com>
2024-12-14 18:38:58 +0000

    Add message generation support

--- freflow/message.py
+++ freflow/message.py
@@ -5,6 +5,7 @@ from typing import Self
 
 
 class Instruction(Enum):
+    UNDEFINED = 0x0000
     COMMAND_A000 = 0xA000
     COMMAND_A001 = 0xA001
     COMMAND_A002 = 0xA002
@@ -24,8 +25,16 @@ class MessageBase(ABC):
     system_id: int
     unknown_0: int
     channel: int
-    instruction: Instruction
-    sequence_number: int
+
+    @abstractmethod
+    def _instruction() -> Instruction:
+        """Instruction
+
+        Returns:
+            Instruction: Instruction value
+        """
+
+        pass
 
     @abstractmethod
     def _variable_data_buffer() -> bytes:
@@ -48,9 +57,9 @@ class MessageBase(ABC):
         data += self.system_id.to_bytes(2, "little")
         data += self.unknown_0.to_bytes(2, "little")
         data += self.channel.to_bytes(2, "little")
-        data += self.instruction.value.to_bytes(2, "little")
-        data += self.sequence_number.to_bytes(2, "little")
+        data += self._instruction().value.to_bytes(2, "little")
         data += self._variable_data_buffer()
+        return data
 
 
 @dataclass
@@ -75,11 +84,175 @@ class GenericMessage(MessageBase):
         channal = int.from_bytes(data[4:6], "little")
         instruction_int = int.from_bytes(data[6:8], "little")
         instruction = Instruction(instruction_int)
-        sequence_number = int.from_bytes(data[8:10], "little")
-        return cls(system_id, unknown_0, channal, instruction, sequence_number)
+        return cls(system_id, unknown_0, channal, instruction)
 
-    def _optional_data_buffer(self) -> bytes:
+    def _instruction() -> Instruction:
+        return Instruction.UNDEFINED
+
+    def _variable_data_buffer(self) -> bytes:
         return self.variable_data
 
-class CommandA001Message(MessageBase):
+
+@dataclass
+class Color:
+    red: int
+    green: int
+    blue: int
+
+    def to_bytes(self) -> bytes:
+        """To Bytes
+
+        Returns:
+            bytes: This instance as bytes
+        """
+
+        data = self.red.to_bytes()
+        data += self.green.to_bytes()
+        data += self.blue.to_bytes()
+        return data
+
+
+class ControlMode(Enum):
+    SET_RF_CONTROLLED_ONLY = 0x00
+    NORMAL = 0x01
+    RESERVED = 0x02
+    RANDOM = 0x03
+
+
+class AnimationMode(Enum):
+    NONE = 0x00
+    BLINK = 0x01
+    FADE = 0x02
+    RESERVED = 0x03
+
+
+@dataclass
+class LightningControl:
+    sequence_number: int
+    control_mode: ControlMode
+    animation_mode: AnimationMode
+    rf_controlled: bool
+    random_mode_sequence_number: int
+    color: Color
+    animation_on_time: int
+    animation_off_time: int
+    brightness: int
+
+    @classmethod
+    def from_bytes(cls, data: bytes) -> Self:
+        """From bytes
+
+        Args:
+            data (bytes): Data Bytes
+
+        Returns:
+            Self: Instance of this class
+        """
+
+        sequence_number = int.from_bytes(data[0:2], "little")
+        flags = data[2]
+        control_mode = ControlMode(flags & 0x03)
+        animation_mode = AnimationMode(flags >> 2 & 0x03)
+        rf_controlled = True if flags >> 4 & 0x03 != 0x00 else False
+        random_mode_sequence_number = data[3]
+        color = Color(
+            data[4],
+            data[5],
+            data[6],
+        )
+        animation_on_time = data[7]
+        animation_off_time = data[8]
+        brightness = data[9]
+        return cls(
+            sequence_number,
+            control_mode,
+            animation_mode,
+            rf_controlled,
+            random_mode_sequence_number,
+            color,
+            animation_on_time,
+            animation_off_time,
+            brightness,
+        )
+
+
+class LightningControlMessage(MessageBase):
+    controls: list[LightningControl]
+
+    @classmethod
+    def from_bytes(cls, data: bytes) -> Self:
+        """From bytes
+
+        Args:
+            data (bytes): Data Bytes
+
+        Returns:
+            Self: Instance of this class
+        """
+
+        generic = GenericMessage.from_bytes(data)
+
+        controls: list[LightningControl] = []
+        for i in range(6):
+            offset = i * 10
+
+            sequence_number = int.from_bytes(
+                generic.variable_data[offset : offset + 2], "little"
+            )
+            flags = generic.variable_data[offset + 2]
+            control_mode = ControlMode(flags & 0x03)
+            animation_mode = AnimationMode(flags >> 2 & 0x03)
+            rf_controlled = True if flags >> 4 & 0x03 != 0x00 else False
+            random_mode_sequence_number = generic.variable_data[offset + 3]
+            color = Color(
+                generic.variable_data[offset + 4],
+                generic.variable_data[offset + 5],
+                generic.variable_data[offset + 6],
+            )
+            animation_on_time = generic.variable_data[offset + 7]
+            animation_off_time = generic.variable_data[offset + 8]
+            brightness = generic.variable_data[offset + 9]
+
+            controls.append(
+                LightningControl(
+                    sequence_number,
+                    control_mode,
+                    animation_mode,
+                    rf_controlled,
+                    random_mode_sequence_number,
+                    color,
+                    animation_on_time,
+                    animation_off_time,
+                    brightness,
+                )
+            )
+
+        return cls(
+            generic.system_id,
+            generic.unknown_0,
+            generic.channel,
+            generic.instruction,
+            controls,
+        )
+
+    def _instruction() -> Instruction:
+        return Instruction.COMMAND_A000
+
+    def _variable_data_buffer(self) -> bytes:
+        data = b""
+        for control in self.controls:
+            data = control.sequence_number.to_bytes(2, "little")
+            flag = control.control_mode.value
+            flag |= control.animation_mode.value << 2
+            flag |= 1 if control.rf_controlled else 0 << 4
+            data += flag.to_bytes()
+            data += control.random_mode_sequence_number.to_bytes()
+            data += control.color.to_bytes()
+            data += control.animation_on_time.to_bytes()
+            data += control.animation_off_time.to_bytes()
+            data += control.brightness.to_bytes()
+        return data
+
+
+class LightningControlMessage2(LightningControlMessage):
     pass
--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 from random import randint
 from time import sleep
 
+from freflow.message import Color, LightningControl, LightningControlMessage
 from freflow.ez_radio_packet import EzRadioPacket
 from freflow.transmitter import Transmitter
 
-payload = bytearray.fromhex(
-    """3C 00 FF FF 00 00 FF 00
-       00 A0 00 00 01 00 00 00 00 04 64 FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
-    """
-)
+# payload = bytearray.fromhex(
+#     """3C 00 FF FF 00 00 FF 00
+#        00 A0 00 00 01 00 00 00 00 04 64 FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+#     """
+# )
 
 frequency_start = int(922.4e6)
 frequency_step = int(200e3)
@@ -20,19 +21,19 @@ for channel in range(channels):
     for i in range(100):
         print("iteration:", i)
 
-        data = b""
-        for j in range(2):
-            iteration = i * 3 + j
-            payload[10] = iteration & 0xFF
-            payload[11] = iteration >> 8 & 0xFF
+    data = b""
+    for j in range(2):
+        iteration = i * 3 + j
 
-            payload[14] = 100 if i % 3 == 0 else 0
-            payload[15] = 100 if i % 3 == 1 else 0
-            payload[16] = 100 if i % 3 == 2 else 0
-            # print(payload[14], payload[15], payload[16])
+        red = 100 if i % 3 == 0 else 0
+        green = 100 if i % 3 == 1 else 0
+        blue = 100 if i % 3 == 2 else 0
 
-            packet = EzRadioPacket(bytes(payload), 4)
-            data += packet.to_bytes()
+        message = LightningControlMessage(0xFFFF, 0x0000, 0x00FF)
+        color = Color(red, green, blue)
+        lightning_control = LightningControl()
+        packet = EzRadioPacket(message.to_bytes(), 8)
+        data += packet.to_bytes()
 
         tx.transmit(data)
         sleep(0.5)
