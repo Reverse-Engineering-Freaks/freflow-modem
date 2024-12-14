25bfa3c0e1a81b3695325cf2902a9878992f1ad7
soltia48 <soltia48@protonmail.com>
2024-12-14 01:21:37 +0000

    The commit

--- freflow/transmitter.py
+++ freflow/transmitter.py
@@ -1,5 +1,6 @@
 from gnuradio import gr, blocks, digital
 import numpy as np
+from python_hackrf import pyhackrf
 from SoapySDR import SOAPY_SDR_TX, SOAPY_SDR_CF32, Device
 from time import sleep
 
@@ -11,10 +12,9 @@ class Transmitter:
     def __init__(
         self,
         tx_device: str,
-        tx_frequency: float,
         tx_sampling_rate: int,
+        tx_frequency: float,
         tx_gain: int,
-        if_gain: int,
     ) -> None:
         """Constructor
 
@@ -30,18 +30,13 @@ class Transmitter:
         self.sdr = Device(dict(driver=tx_device))
         self.sdr.setFrequency(SOAPY_SDR_TX, 0, tx_frequency)
         self.sdr.setSampleRate(SOAPY_SDR_TX, 0, self.tx_sampling_rate)
-
-        if self.sdr.getDriverKey() == "HackRF":
-            self.sdr.setGain(SOAPY_SDR_TX, 0, "VGA", min(max(if_gain, 0.0), 47.0))
-            self.sdr.setGain(SOAPY_SDR_TX, 0, "AMP", 1 if tx_gain > 0 else 0)
-        else:
-            self.sdr.setGain(SOAPY_SDR_TX, 0, tx_gain)
+        self.sdr.setGain(SOAPY_SDR_TX, 0, tx_gain)
 
         self.tb = gr.top_block()
 
         self.src = blocks.vector_source_b([])
 
-        self.samples_per_symbol = tx_sampling_rate // self.__SYMBOLS_PER_SECOND
+        self.samples_per_symbol = self.tx_sampling_rate // self.__SYMBOLS_PER_SECOND
         self.bt = self.__MSK_BT
         self.gmsk_mod = digital.gmsk_mod(
             samples_per_symbol=self.samples_per_symbol,
@@ -56,36 +51,44 @@ class Transmitter:
 
         self.tx_stream = self.sdr.setupStream(SOAPY_SDR_TX, SOAPY_SDR_CF32)
         self.mtu = self.sdr.getStreamMTU(self.tx_stream)
+        self.buffer_wait = self.mtu / tx_sampling_rate
 
         self.sdr.activateStream(self.tx_stream)
-        sleep(0.1)
+        # sleep(1)
 
-    def transmit(self, data: bytes) -> None:
+    def transmit(self, data: bytes) -> int:
         """Transmit data
 
         Args:
             data (bytes): Data
         """
 
-        data = data + b"\x00" * 16
+        # data *= 2
 
         data_bits = np.unpackbits(np.frombuffer(data, dtype=np.uint8))
         self.src.set_data(data_bits.tolist())
         self.sink.reset()
         self.tb.run()
         modulated = np.array(self.sink.data(), dtype=np.complex64)
+        # modulated = np.pad(modulated, (0, self.mtu * 4))
 
-        for i in range(0, len(modulated), self.mtu):
-            chunk = modulated[i : i + self.mtu]
+        sent = 0
+        while sent < len(modulated):
+            chunk = modulated[sent : sent + self.mtu]
             if len(chunk) < self.mtu:
                 chunk = np.pad(chunk, (0, self.mtu - len(chunk)))
-
-            rc = self.sdr.writeStream(self.tx_stream, [chunk], self.mtu)
-            if rc.ret != len(chunk):
-                print(f"Warning: Only {rc.ret} of {len(chunk)} samples sent")
+            status = self.sdr.writeStream(
+                self.tx_stream, [chunk], chunk.size, timeoutUs=1000000
+            )
+            if status.ret != chunk.size:
+                print(f"Warning: Only {status.ret} of {len(chunk)} samples sent")
+                return -1
+            sent += status.ret
+        return sent
 
     def close(self) -> None:
         """Close"""
-        sleep(2)
+        # self.sdr.pyhackrf_close()
+        # sleep(2)
         self.sdr.deactivateStream(self.tx_stream)
         self.sdr.closeStream(self.tx_stream)
---
+++ test.py
@@ -0,0 +1,34 @@
+from random import randint
+from time import sleep
+
+from freflow.ez_radio_packet import EzRadioPacket
+from freflow.transmitter import Transmitter
+
+payload = bytearray.fromhex(
+    """3C 00 FF FF 00 00 FF 00
+       00 A0 00 00 01 00 00 00 00 04 64 FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+    """
+)
+
+tx = Transmitter("hackrf", int(4e6), int(922.4e6), 61)
+for i in range(100):
+    print(i)
+
+    data = b""
+    for j in range(2):
+        iteration = i * 3 + j
+        payload[10] = iteration & 0xFF
+        payload[11] = iteration >> 8 & 0xFF
+
+        payload[14] = 100 if i % 3 == 0 else 0
+        payload[15] = 100 if i % 3 == 1 else 0
+        payload[16] = 100 if i % 3 == 2 else 0
+        print(payload[14], payload[15], payload[16])
+
+        packet = EzRadioPacket(bytes(payload), 3)
+        data += packet.to_bytes()
+
+    tx.transmit(data)
+    sleep(0.5)
+
+tx.close()
